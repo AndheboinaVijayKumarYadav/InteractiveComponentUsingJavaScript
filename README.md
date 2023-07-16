af9e8a92d7f20b6ad6b182ad7f9eefe308f00217
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2023-07-16 14:47:12 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # InteractiveComponentUsingJavaScript
 In this repo I am going to create a interactive component which ask for feedback and responds according using HTML ,CSS and JS
+
+
+# what I have learned
+structuring the html and using appropiate tags for example
+#1 instead of using five div's elements for rating list I would have use unordered list ul 
+<ul> 
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+</ul>
+#2 Proper naming of the elements like id ,class etc
+#3 in CSS I have how to use custom properties effectively , to reduce code by using child selectors exp
+.component > * {} selector . 
+#4 Proper utilization of flex box concept 
+#5 use of media queries 
+coming to Javascript
+#6 to properyly select the dom elements using querySelector and querySelectorAll
+#7 to divide the code properly into pieces using functions
+#8 better understanding of NodeList
--- script.js
+++ script.js
@@ -5,18 +5,28 @@ const ratingOutput = document.querySelector('#rating-output');
 const thankyouComponent = document.querySelector('.thankyou-component');
 const componentMain = document.querySelector('.component');
 
+// event listeners
 ratingList.forEach((element) => {
-    element.addEventListener('click', () => {
-        // remove the active class for list
-        for(var i=0;i<5;i++){
-            ratingList[i].classList.remove('active');
-        }
-        element.classList.add('active');
-    } )
-} )
+    element.addEventListener('click',() => {
+        setActiveClass(element);
+    });
+} );
 
-submitButton.addEventListener('click', () => {
+submitButton.addEventListener('click',displayResult);
+
+// functions
+function setActiveClass(element) {
+     // remove the active class for list if any 
+     for(var i=0;i<5;i++){
+        ratingList[i].classList.remove('active');
+    }
+    // here we can also use array forEach method for removing the class i.e ratingList.forEach((rating) => rating.classList.remove('active'));
+    element.classList.add('active')
+}
+
+function displayResult(){
     let active = document.querySelector('.active');
+    // checking here if any rating is selected or not if not it will throw an alert
     if(!active){
         alert("Please enter valid rating");
         return;
@@ -26,5 +36,6 @@ submitButton.addEventListener('click', () => {
     ratingOutput.innerText = `You have selected ${selectedRatingValue} out of 5`;
     thankyouComponent.style.display = 'flex';
     componentMain.style.display ='none';
+}
 
-});
+
